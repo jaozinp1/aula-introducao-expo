8e1eff81d98ca07bbd3235929b3cd1127db3dcee
jaozinp1 <joaohenriqeu23@gmail.com>
2025-05-26 21:34:54 -0300

    aaaa
    aaaaaaa

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import Exemplo03 from './src/exemplos/exemplo-03';
 import Exemplo04 from './src/exemplos/exemplo-04';
 import Exemplo05 from './src/exemplos/exemplo-05';
 import Exemplo06 from './src/exemplos/exemplo-06';
+import Exemplo07 from './src/exemplos/exemplo-07';
+import Exemplo08 from './src/exemplos/exemplo-08';
 
 //Atividades
 import Atividade01 from './src/atividades/atividade-01';
@@ -18,10 +20,11 @@ import Atividade03 from './src/atividades/atividade-03';
 import Atividade04 from './src/atividades/atividade-04';
 import Atividade05 from './src/atividades/atividade-05';
 import Atividade06 from './src/atividades/atividade-06';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade06 />
+      <Exemplo08 />
       <StatusBar style="auto" />
     </View>
   );
